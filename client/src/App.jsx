55551d2bd39b14682efae0005557ebf4a1091f9d
IrinaJ <irina.ul87@gmail.com>
2024-03-22 11:47:18 -0400

    cleint added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './app.css'
+import logo from './assets/sql-server.png'
+import {useState} from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [prompt, setPrompt] = useState('')
+  const handleOnSubmit = (e) => {
+    e.preventDefault()
+    console.log('Form submited with request', prompt)
+    
+    // sendPromptToServer(prompt)
+  }
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main className='body'>
+      <img src={logo} alt='logo' className='icon'/>
+      <h3>Don&#39;t know SQL? Well, you don&#39;t have to!</h3>
+
+      <form onSubmit={handleOnSubmit}>
+        <input type='text' name='query-description' placeholder='Describe your query' 
+               onChange={(e) => setPrompt(e.target.value)}/>
+        <input type='submit' value='Get SQL query'/>
+      </form>
+    </main>
   )
 }
 
